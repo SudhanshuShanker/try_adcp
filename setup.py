f54b2dec3fe682a59952e016b512026a835a9461
Sudhanshu Shanker <sudhanshu.bioinfo@gmail.com>
2023-05-15 18:01:51 -0700

    with dependencies

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
      packages=find_namespace_packages(
         where='src',exclude='dist'),
      package_dir = {'': 'src'},
-    # install_requires =["biopython>=1.76.0", "numpy>=1.18.0", "openbabel>=2.4.1","python >=3.7"],
+    # install_requires =["biopython>=1.76.0", "numpy>=1.18.0", "openbabel>=2.4.1","python >=3.6"],
 
 )
