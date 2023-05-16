1a708f54981cc6c433de30da28963d47371d0b5c
Sudhanshu Shanker <sudhanshu.bioinfo@gmail.com>
2023-05-15 17:21:28 -0700

    with dependencies

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ from setuptools import find_namespace_packages
 
 setup(
      name='ADCP',
-     scripts=['bin_scripts/adcp'], 
+     scripts=['./bin_scripts/adcp'], 
      data_files = [('bin',['./bin_scripts/adcp_Linux-x86_64_1.1']),
                    ('lib',['./lib_files/libnlopt.so.0']),
                    ],
      packages=find_namespace_packages(
         where='src',exclude='dist'),
      package_dir = {'': 'src'},
-    # install_requires =['pybel==0.15.0', 'biopython==1.76','colorama','numpy==1.18.5'],
-
+    # install_requires =["biopython>=1.76.0", "numpy>=1.18.0", "openbabel>=2.4.1","python >=3.7"],
 
 )
